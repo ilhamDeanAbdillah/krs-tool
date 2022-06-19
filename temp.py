1f3189378d26d3708c21349cb1ae58c2f71ab5e6
ILHAMDEANABDILLAH <deanabdillah@gmail.com>
2022-06-19 10:22:39 +0700

    delete

--- temp.py
+++
@@ -1,4 +0,0 @@
-dataBase = [(5, '1', 'senin', '09:47', '11:47'), (6, '2', 'senin', '10:48', '12:48'), (7, '1', 'selasa', '09:49', '09:49'), (8, 'aaa', 'senin', '10:51', '13:51'), (9, 'fddfdaf', 'senin', '10:52', '12:52')]
-
-for data in dataBase:
-    print(data[2])
